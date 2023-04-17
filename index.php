e9b82f62bd16dddf5cd67f899be813ec5a030024
Shrikant Patil <119443416+shrikantpatil2204@users.noreply.github.com>
2023-04-17 11:21:57 +0530

    Tool (#7)
    * sixth commit
    
    * seventh commit
    
    * 8th commit

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 
 <h1>My PHP page using openshift S2I </h1>
  <h2>This is updated page</h2>
-  <h3>updated file</h3>
+  <h4>commit the code</h4>
+   <h6>this is final code</h6>
 <?php
 echo "Hello from shri";
 ?>
